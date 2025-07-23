4424773c423061b153dd0757be6131afc41aa3a6
Alik05-WW <tlepbergenov.a05@gmail.com>
2025-07-23 17:18:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import re
 
 # === КОНФИГ ===
 TELEGRAM_TOKEN = "8047539836:AAGuwECC3Ee53GI-QhBb_oACAZAneicb9Z0"
-AI_API_KEY = "io-v2-eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9..."  # вставь полный API ключ
+AI_API_KEY = "io-v2-eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lciI6IjBlMjM5MDg5LWE4MTItNGFiYS05OWI1LTMyNjI5NGVjNzQ2MyIsImV4cCI6NDkwNjc2NDEwMH0.Gm1wXrIoo53lYABUz4rHg7l6rYPRhECMp5pLNVNqrPmiz13jVq6LWnvUu1xP9A7WHToIp4AJCfDHhhW3Oa1f1g
+"
 AI_URL = "https://api.intelligence.io.solutions/api/v1/chat/completions"
 MODEL = "deepseek-ai/DeepSeek-R1-0528"
 
